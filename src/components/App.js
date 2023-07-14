e5aa2e6dc2334fe2fa808475c1f3d038bd0b8b6a
Harishprb28 <50201375+Harishprb28@users.noreply.github.com>
2023-07-14 15:23:24 +0530

    final commit

--- src/components/App.js
+++ src/components/App.js
@@ -2,7 +2,7 @@ import React, { useState } from "react";
 import "./../styles/App.css";
 
 const App = () => {
-  const [count, setCount] = useState;
+  const [count, setCount] = useState(1);
   const handleClick = () => {
     setCount(count + 1);
   };
