a3a1db81316682ae979d325b5ab5c23bb38d16fd
Harishprb28 <50201375+Harishprb28@users.noreply.github.com>
2023-07-14 15:10:13 +0530

    commit changes

--- src/components/App.js
+++ src/components/App.js
@@ -1,18 +1,17 @@
-
 import React, { useState } from "react";
-import './../styles/App.css';
+import "./../styles/App.css";
 
 const App = () => {
-  const[count , setCount] = useState;
-  const handleClick =()=>{
-    setButtonClick(count + 1)
-  }
+  const [count, setCount] = useState;
+  const handleClick = () => {
+    setButtonClick(count + 1);
+  };
   return (
     <div>
-       <p>Button Clicked {count} times </p>
-       <button onClick={handleClick}>Click Me</button>
+      <p>Button Clicked {count} times </p>
+      <button onClick={handleClick}>Click Me</button>
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
